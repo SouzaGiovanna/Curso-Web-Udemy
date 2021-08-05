495b15cadbbae82fce1838ea7a34574b802d5847
SouzaGiovanna <gi.souza03@gmail.com>
2021-08-05 12:45:34 -0300

    capítulo de node parte 1

---
+++ exercicios-js/node/moduloB.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite(){
+        return 'Boa noite'
+    }
+}
+
+let a = 'aaaaaaaaaaaaaaaaaa'
---
+++ exercicios-js/node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,9 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+
+const saudacao = require('saudacao')
+console.log(saudacao.msg)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
